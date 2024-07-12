1d38163b48084c8cfabaf3821287796f547d1dae
TrueSgtMonkey <codythompsonwp4@gmail.com>
2024-07-12 00:22:50 -0700

    fix status.log not being created from git command

--- options/git_status_rebase.py
+++ options/git_status_rebase.py
@@ -1,13 +1,20 @@
 from git_path import GitPath
 from git_save import GitSaveVars
 from style import Color
+import os as os
+
+STATUS_FILE_NAME: str = "status.log"
+MAX_STATUS_RETRIES: int = 5
 
 class GitStatusRebase:
     @classmethod
     def git_status_rebase(self, git_path: GitPath, git_save_vars: GitSaveVars):
         # checking if there are local changes that need to be dealt with
-        git_path.cmd_at_path("git status > \"" + git_path.curr_dir + "\status.log\"")
-        status_file = open("status.log", "r")
+        git_path.cmd_at_path("git status > \"" + git_path.curr_dir + f"\{STATUS_FILE_NAME}\"")
+        if not self.was_status_file_created_correctly(git_path):
+            return
+
+        status_file = open(STATUS_FILE_NAME, "r")
         cannot_rebase = True
         for line in status_file.readlines():
             line = line.strip()
@@ -29,3 +36,19 @@ class GitStatusRebase:
         # if local changes dealt with, perform a fetch then rebase
         git_path.cmd_at_path("git remote update")
         git_path.cmd_at_path("git rebase " + git_save_vars.rebase_branch)
+
+    @classmethod
+    def was_status_file_created_correctly(self, git_path: GitPath) -> bool:
+        retries: int = 0
+
+        while not os.path.isfile(STATUS_FILE_NAME) and retries < MAX_STATUS_RETRIES:
+            os.system("touch " + STATUS_FILE_NAME)
+            Color.print(Color.RED, "Cannot obtain status! " + STATUS_FILE_NAME + " does not exist!\nRetrying...")
+            git_path.cmd_at_path("git status > \"" + git_path.curr_dir + f"\{STATUS_FILE_NAME}\"")
+            retries += 1
+
+        if retries >= MAX_STATUS_RETRIES:
+            Color.print(Color.RED, f"Retries failed. Cannot retrieve {STATUS_FILE_NAME}.\nAborting...")
+            return False
+
+        return True
